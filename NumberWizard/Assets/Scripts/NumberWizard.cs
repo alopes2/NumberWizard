8e442f2f59f0b679f73f68c9f1fb2fedf1575e0b
Andre Lopes <andrelo@dunnhumby.com>
2020-02-13 16:15:32 +0100

    Project commit

---
+++ NumberWizard/Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour
+{
+    [SerializeField]
+    int _max;
+
+    [SerializeField]
+    int _min;
+
+    private int _guess;
+
+    [SerializeField]
+    private TextMeshProUGUI _guessText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartGame();
+    }
+
+    public void OnPressHigher()
+    {
+        if (_min < _max && _guess != _max)
+        {
+            _min = _guess + 1;
+            NextGuess();
+        }
+    }
+
+    public void OnPressLower()
+    {
+        if (_min < _max)
+        {
+            _max = _guess - 1;
+            NextGuess();
+        }
+    }
+
+    public void OnPressSuccess()
+    {
+
+    }
+
+    private void StartGame()
+    {
+        NextGuess();
+    }
+
+    private void NextGuess()
+    {
+        _guess = Random.Range(_min, _max + 1);
+        _guessText.text = _guess.ToString();
+    }
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+# Number Wizard
+
+This is a game project for learning Unity framework
+It is a game where you think of a number and it should start guessing your number by asking if it is higher or lower than the current guess
+
+## Resources
+
+Useful resources used here
+
+* **DaFont** - [https://www.dafont.com/](https://www.dafont.com/)
+* Images from **Pixabay** - [https://pixabay.com/](https://pixabay.com/)
